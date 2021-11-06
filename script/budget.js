c614d768fd9133a224f9ed2562defc3a97c78628
Ruth Wu <rwu02@email.unc.edu>
2021-11-06 12:49:00 -0400

    Update budget.js

---
+++ script/budget.js
@@ -0,0 +1,33 @@
+const Category = {
+    FOOD: 0,
+    UTILITIES: 1,
+    ENTERTAINMENT: 2
+}
+
+class AmountData {
+    constructor() {
+        this.data = [];
+    }
+
+    add(value) {
+        data[Date.getTime()] = value;
+    }
+
+    total() {
+        
+    }
+
+    weeklyTotal() {
+
+    }
+
+    monthlyTotal() {
+        
+    }
+}
+
+const Food = {
+    amountData: new AmountData,
+    budget: 0,
+    category: Category.FOOD,
+}
