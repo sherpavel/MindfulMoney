5411dbdd61855011b3951433ebf82afe790bb315
Pavel Sher <54330842+sherpavel@users.noreply.github.com>
2021-11-06 12:29:45 -0400

    outline

--- script/main.js
+++ script/main.js
@@ -1,3 +1,31 @@
 window.onload = () => {
+    main();
+}
 
+const EXPENSES_TYPE = {
+    FOOD: 0,
+    UTILITY: 1,
+    ENT: 2,
+    CUSTOM: 3,
+}
+
+function main() {
+    let testArray = {
+
+    };
+
+    testArray.forEach(budget => {
+
+    });
+}
+
+
+function showBudget(obj) {
+    let JBudget = $("<div>");
+
+    let icon = $("img");
+
+
+
+    return JBudget;
 }
