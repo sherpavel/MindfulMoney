19fdcb3827f8993c88698e851c76058ce3191b73
Ruth Wu <rwu02@email.unc.edu>
2021-11-07 09:38:10 -0500

    chart

--- script/plot.js
+++ script/plot.js
@@ -1,3 +1,7 @@
+src="https://www.gstatic.com/charts/loader.js%22%3E"
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
 function initPlot() {
     // var data = [{
     //     values: [typeTotal(CATEGORY.FOOD),typeTotal(CATEGORY.UTILITIES),typeTotal(CATEGORY.ENTERTAINMENT),typeTotal(CATEGORY.CUSTOM)],
@@ -11,4 +15,20 @@ function initPlot() {
     // };
     
     // Plotly.newPlot('pie', data, layout);
+
+    var data = google.visualization.arrayToDataTable([
+        ['CATEGORY', 'AMOUNT SPENT'],
+        ['Food',     typeTotal(CATEGORY.FOOD)],
+        ['Utilities',      typeTotal(CATEGORY.UTILITIES)],
+        ['Entertainment',  typeTotal(CATEGORY.ENTERTAINMENT)],
+        ['Custom', typeTotal(CATEGORY.CUSTOM)],
+        ]);
+
+    var options = {
+        title: 'Spending Report',
+        colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f']
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(data, options); 
 }
